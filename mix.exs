95cc53b91f30199a15c39846caedbedeb0a9217f
Vignesh Rajagopalan <me@aarvay.com>
2016-05-03 17:59:49 +0530

    Add package and desc info
    Preparing for a hex push

--- mix.exs
+++ mix.exs
@@ -7,27 +7,28 @@ defmodule Plivo.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:httpoison, "~> 0.8.0"},
      {:poison, "~> 2.1.0"}]
+  end
+
+  defp description do
+    "An elixir client for Plivo API"
+  end
+
+  defp package do
+    [name: :plivo,
+     maintainers: ["Vignesh Rajagopalan"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/aarvay/plivo-elixir"}]
   end
 end
